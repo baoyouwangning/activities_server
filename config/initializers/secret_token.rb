482f87f6e87e7b40a96041d8ff939b6cc3f80e3f
= <=>
2014-07-24 16:51:15 +0800

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActivitiesServer::Application.config.secret_key_base = '4dccce470459c5d7376d9f8d8ccb038eab50afaef6d97810830292eaa5498e3605fc62800a96126f8ba67b1a53abeb832bb8a7eeb8a777087ddca5c6ad556804'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ActivitiesServer::Application.config.session_store :cookie_store, key: '_activities_server_session'
